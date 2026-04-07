66877be63a3b29864c5053e8f63ea89a313db48f
Geekasso <geekasso@gmail.com>
2026-04-07 17:06:27 -0400

    Release v0.4.23 - Fix magic login email markdown indentation rendering raw HTML

--- resources/views/emails/magic-login.blade.php
+++ resources/views/emails/magic-login.blade.php
@@ -1,14 +1,14 @@
 <x-mail::message>
-    # Login to AdmixCentral
+# Login to AdmixCentral
 
-    Click the button below to log in to your account. This link will expire in 15 minutes.
+Click the button below to log in to your account. This link will expire in 15 minutes.
 
-    <x-mail::button :url="$url">
-        Log in securely
-    </x-mail::button>
+<x-mail::button :url="$url">
+Log in securely
+</x-mail::button>
 
-    If you didn't request this email, you can safely ignore it.
+If you didn't request this email, you can safely ignore it.
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 </x-mail::message>
